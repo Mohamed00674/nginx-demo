0c4cb4b22ff425d978abd12bfed8902b82fc0c10
Mohamed ahmed <ma614351@gmail.com>
2025-04-01 15:30:45 +0100

    load balancer

--- server.js
+++ server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
 
-const appName = process.env.APP_NAME
+dotenv.config()
+
+const replicaApp = process.env.APP_NAME
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${appName}`);
+    console.log(`Request served by ${replicaApp}`);
 });
 
 app.listen(port, () => {
-    console.log(`${appName} is listening on port ${port}`);
+    console.log(`${replicaApp} is listening on port ${port}`);
 });
